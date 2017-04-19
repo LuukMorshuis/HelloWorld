2cd5ea70436db514a05311f40497e6144db4b144
LuukMorshuis <llmorshu@avans.nl>
2017-04-19 09:39:47 +0200

    My first commit

---
+++ desktop/prog1/gittest/src/gittest/Mainn.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class Mainn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("casey wat kijk je jonge");
+	}
+
+}
